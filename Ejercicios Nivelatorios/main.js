d2b823037720348968425cf1076379322e63ef26
JudithGrau <judith.betina.grau@gmail.com>
2022-04-21 14:25:49 -0300

    Agrego ejercicios nivelatorios

---
+++ Ejercicios Nivelatorios/main.js
@@ -0,0 +1,48 @@
+//backticks ( `` )
+//------------------------------------------------------Ejercicio 1---------------------------------------------------------------------------------
+// declarar una variable que se llame nombre
+//el dato que le pasamos a la variable se lo pedimos al usuario en un prompt
+//mostramos un alerta con la cantidad de caracteres del nombre que ingreso el usuario/a
+
+/* let nombre = prompt("Cual es tu nombre?");
+alert(nombre.length); */
+
+//------------------------------------------------------Ejercicio 2---------------------------------------------------------------------------------
+//pido un nombre al usuario/a
+//pido al usuario/a que ingrese un numero
+//hola "nombre de usuaria/o te pedimos que ingreses un numero"
+//alerta con "nombreusuaria/o" tiene (numero de letras) caracteres; 
+//le sumamos el numero que ingresaste el resultado es (numero de letras + numero ingresado)
+
+/* let nombre = prompt("Cual es tu nombre?");
+let num1 = Number(prompt(`Hola ${nombre} Ingrese un número`));
+let resultado = num1 + Number(nombre.length)
+alert(`${nombre} tiene ${nombre.length} caracteres. la suma numero ingresado + la cantidad de letras de tu nombre es: ${resultado}`); */
+
+//------------------------------------------------------Ejercicio 3---------------------------------------------------------------------------------
+// Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga «Hello Arbi».
+
+/* alert("Hello Arbi"); */
+
+//------------------------------------------------------Ejercicio 4---------------------------------------------------------------------------------
+//Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello Arbi» (document.write).
+
+/* document.write("Hello Arbi"); */
+
+//------------------------------------------------------Ejercicio 5---------------------------------------------------------------------------------
+//Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+
+/* document.write(3 + 5); */
+
+//------------------------------------------------------Ejercicio 6---------------------------------------------------------------------------------
+//Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola (nombre)»
+
+/* let nombre = prompt("Cual es tu nombre?");
+document.write(`Hola ${nombre}`); */
+
+//------------------------------------------------------Ejercicio 7---------------------------------------------------------------------------------
+// Escribe un programa de dos lineas que pida el nombre al usuario con un prompt y escriba un texto que diga «Hola (nombre) tu nombre tiene (cantidad) letras»
+
+/* let nombre = prompt("Cual es tu nombre?");
+document.write(`Hola ${nombre}, tu nombre tiene ${nombre.length} letras.`); */
+
